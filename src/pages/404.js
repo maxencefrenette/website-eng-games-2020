eb2f3c8a9b02f2ec13fe9f67b090f131bc046d36
Maxence Frenette <maxence.frenette@gmail.com>
2019-04-03 17:31:43 -0400

    Improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Article from "../components/Article";
 
 const NotFoundPage = () => (
-  <div>
+  <Article>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  </Article>
 );
 
 export default NotFoundPage;
