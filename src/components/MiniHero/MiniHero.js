1f3c4532f3a3521fdd019ddcd72a87f8f35c566e
Maxence Frenette <maxence.frenette@gmail.com>
2019-04-03 20:56:34 -0400

    Migrate MiniHero to styled-components

--- src/components/MiniHero/MiniHero.js
+++ src/components/MiniHero/MiniHero.js
@@ -1,90 +1,62 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
+import styled from "styled-components";
+import theme from "../../theme/theme2.yaml";
+
+const MiniHeroStyle = styled.section`
+  align-items: center;
+  background: linear-gradient(0deg, #e0306e, #6438b5);
+  background-image: url(${props => props.backgrounds.mobile.src});
+  background-size: cover;
+  background-position: center center;
+  color: white;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  min-height: 400px;
+  height: 400px;
+  padding: 40px;
+  padding-top: 100px;
+
+  & h1 {
+    text-align: center;
+    font-size: 2.2em;
+    margin: 0 0 40px 0;
+    color: white;
+    line-height: 1.1;
+    text-remove-gap: both 0 ${theme.font.heading};
+  }
+
+  @media ${theme.tablet} {
+    background-image: url(${props => props.backgrounds.tablet.src});
+    & h1 {
+      max-width: 90%;
+      font-size: ${`calc(1.7em * 1.3)`};
+    }
+  }
+
+  @media ${theme.desktop} {
+    background-image: url(${props => props.backgrounds.desktop.src});
+    & h1 {
+      max-width: 80%;
+      font-size: ${`calc(1.7em * 1.5)`};
+    }
+  }
+`;
 
 const MiniHero = props => {
-  const { backgrounds, theme, children } = props;
+  const { backgrounds, children } = props;
 
   return (
-    <React.Fragment>
-      <section className="mini-hero">
-        <h1>{children}</h1>
-      </section>
-
-      {/* --- STYLES --- */}
-      <style jsx>{`
-        .mini-hero {
-          align-items: center;
-          background: ${theme.hero.background};
-          background-image: url(${backgrounds.mobile.src});
-          background-size: cover;
-          background-position: center center;
-          color: ${theme.text.color.primary.inverse};
-          display: flex;
-          flex-flow: column nowrap;
-          justify-content: center;
-          min-height: 400px;
-          height: 400px;
-          padding: ${theme.space.inset.l};
-          padding-top: ${theme.header.height.homepage};
-        }
-
-        .mini-hero h1 {
-          text-align: center;
-          font-size: ${theme.hero.h1.size};
-          margin: ${theme.space.stack.l};
-          color: ${theme.hero.h1.color};
-          line-height: ${theme.hero.h1.lineHeight};
-          text-remove-gap: both 0 "Open Sans";
-
-          :global(strong) {
-            position: relative;
-
-            &::after,
-            &::before {
-              content: "›";
-              color: ${theme.text.color.attention};
-              margin: 0 ${theme.space.xs} 0 0;
-              text-shadow: 0 0 ${theme.space.s} ${theme.color.neutral.gray.k};
-            }
-            &::after {
-              content: "‹";
-              margin: 0 0 0 ${theme.space.xs};
-            }
-          }
-
-          & > small {
-            font-size: 0.7em;
-          }
-        }
-
-        @from-width tablet {
-          .mini-hero {
-            background-image: url(${backgrounds.tablet.src});
-            h1 {
-              max-width: 90%;
-              font-size: ${`calc(${theme.hero.h1.size} * 1.3)`};
-            }
-          }
-        }
-
-        @from-width desktop {
-          .mini-hero {
-            background-image: url(${backgrounds.desktop.src});
-            h1 {
-              max-width: 80%;
-              font-size: ${`calc(${theme.hero.h1.size} * 1.5)`};
-            }
-          }
-        }
-      `}</style>
-    </React.Fragment>
+    <MiniHeroStyle backgrounds={backgrounds}>
+      <h1>{children}</h1>
+    </MiniHeroStyle>
   );
 };
 
 MiniHero.propTypes = {
   backgrounds: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
 };
 
 export const query = graphql`
