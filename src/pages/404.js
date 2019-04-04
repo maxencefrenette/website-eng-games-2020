b996593ff6b784c79098382d4bcd850c64c28d84
Maxence Frenette <maxence.frenette@gmail.com>
2019-04-03 22:48:22 -0400

    Add image to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Article from "../components/Article";
+import MiniHero from "../components/MiniHero";
 
-const NotFoundPage = () => (
-  <Article>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Article>
-);
+const NotFoundPage = props => {
+  const {
+    data: { backgrounds }
+  } = props;
+
+  return (
+    <>
+      <MiniHero backgrounds={backgrounds} />
+      <Article>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Article>
+    </>
+  );
+};
 
 export default NotFoundPage;
+
+export const query = graphql`
+  query NotFoundPageQuery {
+    backgrounds: imageSharp(fluid: { originalName: { regex: "/volley/" } }) {
+      ...MiniHero
+    }
+  }
+`;
