44c9ea62f4fae935dd2c0fed7cc31dc7153e28fe
Maxence Frenette <maxence.frenette@gmail.com>
2019-05-12 14:33:46 -0400

    Tweak hero text and logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,7 @@ import theme from "../theme/theme.yaml";
 import styled from "styled-components";
 import Img from "gatsby-image";
 import { graphql } from "gatsby";
+import { ScreenWidthContext } from "../layouts";
 
 const HeroContainer = styled.section`
   align-items: center;
@@ -32,8 +33,16 @@ const HeroContainer = styled.section`
 
 const Header = styled.h1`
   text-align: center;
-  margin: 0 0 40px 0;
   color: white;
+  margin-bottom: 15px;
+
+  @media ${theme.tablet} {
+    margin-bottom: 20px;
+  }
+
+  @media ${theme.desktop} {
+    margin-bottom: 30px;
+  }
 `;
 
 const Line = styled.span`
@@ -58,7 +67,7 @@ const Hero = props => {
   return (
     <HeroContainer backgrounds={backgrounds}>
       <Header>
-        <Line ratio={0.3}>
+        <Line ratio={0.45}>
           <FormattedMessage id="30th-edition" />
         </Line>
         <br />
@@ -69,7 +78,15 @@ const Hero = props => {
         <Line ratio={2.2}>2020</Line>
       </Header>
 
-      <Img fixed={data.logoHero.fixed} />
+      <ScreenWidthContext.Consumer>
+        {width => {
+          if (width < 600) {
+            return <Img fixed={data.logoHeroMobile.fixed} />;
+          } else {
+            return <Img fixed={data.logoHero.fixed} />;
+          }
+        }}
+      </ScreenWidthContext.Consumer>
     </HeroContainer>
   );
 };
@@ -83,7 +100,12 @@ export default Hero;
 export const query = graphql`
   fragment Hero on Query {
     logoHero: imageSharp(fluid: { originalName: { regex: "/logo-2020/" } }) {
-      fixed(width: 250, height: 250, quality: 90, cropFocus: CENTER) {
+      fixed(width: 200, height: 200, quality: 90, cropFocus: CENTER) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+    logoHeroMobile: imageSharp(fluid: { originalName: { regex: "/logo-2020/" } }) {
+      fixed(width: 150, height: 150, quality: 90, cropFocus: CENTER) {
         ...GatsbyImageSharpFixed
       }
     }
