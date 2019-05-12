edc941bfb5bfdb68c4e641ec38d27d147cf1b3bd
AlyssaBk <alyssa.bouchenak@gmail.com>
2019-05-12 12:08:07 -0400

    updated competition banner pic

--- src/pages/competitions.js
+++ src/pages/competitions.js
@@ -140,7 +140,7 @@ export const query = graphql`
       }
     }
 
-    backgrounds: imageSharp(fluid: { originalName: { regex: "/volley/" } }) {
+    backgrounds: imageSharp(fluid: { originalName: { regex: "/competition/" } }) {
       ...MiniHero
     }
 
