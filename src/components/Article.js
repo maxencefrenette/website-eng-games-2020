3e46403178ab9bfaebab30b253b7633b5eb0c904
Maxence Frenette <maxence.frenette@gmail.com>
2019-05-19 23:34:43 -0400

    Scape paper overlap with vertical screen size

--- src/components/Article.js
+++ src/components/Article.js
@@ -131,7 +131,7 @@ const Styles = styled.article`
 const Paper = styled.div`
   min-height: 400px;
   padding: 8px;
-  margin-top: -100px;
+  margin-top: -8vh;
 
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
     0px 2px 1px -1px rgba(0, 0, 0, 0.12);
