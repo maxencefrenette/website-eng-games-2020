3c20e0a9fcaf59a2dd0955a713320dd674cf0795
Maxence Frenette <maxence.frenette@gmail.com>
2019-06-28 08:44:49 -0400

    Update preview image

--- content/meta/config.js
+++ content/meta/config.js
@@ -10,31 +10,12 @@ module.exports = {
 
   /* author */
   authorName: "Maxence Frenette",
-  authorTwitterAccount: "greglobinski",
-
-  /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
-  manifestStartUrl: "/index.html",
+  manifestName: "Jeux de Génie du Québec 2020",
+  manifestShortName: "JdG 2020", // max 12 characters
+  manifestStartUrl: "/",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
-  manifestDisplay: "standalone",
-
-  // gravatar
-  // Use your Gravatar image. If empty then will use src/images/jpg/avatar.jpg
-  // Replace your email adress with md5-code.
-  // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
-  // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
-
-  // social
-  authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
-  ]
+  manifestDisplay: "standalone"
 };
